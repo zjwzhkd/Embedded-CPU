61aee0dd525cc014d49b4fd7db9a3e7d2cc39e34
Keda Huang <huangkeda88@163.com>
2019-06-28 01:03:20 +0800

    stm8s: 修改延时函数

--- arch/stm8/stm8s/cpu_tick.c
+++ arch/stm8/stm8s/cpu_tick.c
@@ -8,13 +8,6 @@
 *******************************************************************************/
 
 #include "cpu_tick.h"
-/*******************************************************************************
-
-                                    全局变量
-
-*******************************************************************************/
-static ListHead_t cpuTickIRQList;
-static uint32_t fac_ms = 0;
 
 /*******************************************************************************
 
@@ -24,11 +17,6 @@ static uint32_t fac_ms = 0;
 /*CPU节拍以及时间管理初始化*/
 void cpu_TickInit(void)
 {
-#ifdef CPU_TICK_PERIOD_IS_1MS
-    CPU_Assert(CPU_TICK_HZ == 1000);
-#endif
-    list_Init(&cpuTickIRQList);
-    fac_ms = CPU_TIMER_HZ/1000;
     /*
      * 初始化Timer4
      * CPU_TIMER_HZ = CPU_FREQ_HZ/64
@@ -38,62 +26,6 @@ void cpu_TickInit(void)
     TIM4_TimeBaseInit(TIM4_PRESCALER_64, CPU_TIMER_HZ/CPU_TICK_HZ-1);
     TIM4_ITConfig(TIM4_IT_UPDATE, ENABLE);
     TIM4_Cmd(ENABLE);
-}
-
-/**
- * 注册节拍中断请求
- *
- * @param irq: 待注册的节拍中断请求的结构体指针
- *
- * @param period: 节拍中断请求的周期
- *
- * @param isr: 节拍中断请求的处理函数指针
- */
-void cpu_TickIRQRegister(TickIRQ_t *irq, tick_t period, TickIRQHandler_t isr)
-{
-cpu_t cpu_sr;
-
-    /*参数检验*/
-    CPU_Assert(0 != period);
-    CPU_Assert(0 != isr);
-    /*注册节拍中断*/
-    list_Init(&irq->node);
-    irq->period = period;
-    irq->count  = period-1;
-    irq->isr    = isr;
-    cpu_sr = CPU_EnterCritical();
-    {
-        list_Add(&cpuTickIRQList, &irq->node);
-    }
-    CPU_ExitCritical(cpu_sr);
-}
-
-/**
- * (总)节拍中断处理函数
- *
- * @note: 以CPU_TICK_HZ的频率, 在CPU节拍中断函数中调用
- */
-void cpu_TickHandler(void)
-{
-ListNode_t *pos;
-TickIRQ_t  *irq;
-
-    list_for_each(pos, &cpuTickIRQList)
-    {
-        irq = list_entry(pos, TickIRQ_t, node);
-        if (irq->period > 0)
-        {
-            if (irq->count > 0)
-            {
-                irq->count--;
-            }
-            else
-            {
-                irq->count = irq->period - 1;
-                (irq->isr)();
-            }
-        }
-    }
 }
 
 /*******************************************************************************
@@ -123,17 +55,4 @@ uint8_t tnew, told, reload = TIM4->ARR + 1;
             if (tcnt>=totalCnt) break;
         }
     }
-}
-
-/**
- * 毫秒级延时函数
- *
- * @param nms: 延时时间(ms)
- */
-void cpu_DelayMs(uint16_t nms)
-{
-uint32_t tmp_n;
-
-    tmp_n = (uint32_t)fac_ms*nms;
-    cpu_Delay(tmp_n);
 }
--- arch/stm8/stm8s/include/cpu_tick.h
+++ arch/stm8/stm8s/include/cpu_tick.h
@@ -12,35 +12,17 @@
 
 /* 头文件 --------------------------------------------------------------------*/
 #include "cpu_port.h"
-#include "cpulib_list.h"
-
-/* 数据结构 ------------------------------------------------------------------*/
-/*节拍处理函数类型*/
-typedef void (*TickIRQHandler_t) (void);
-/*节拍中断请求结构体类型*/
-typedef struct tick_irq TickIRQ_t;
-struct tick_irq
-{
-    tick_t              period; /*产生节拍的周期  */
-    tick_t volatile     count;  /*内部计数器      */
-    TickIRQHandler_t    isr;    /*节拍中断服务函数*/
-    ListNode_t          node;   /*节拍链表结点    */
-};
 
 /* 节拍转换宏 ----------------------------------------------------------------*/
-#ifdef CPU_TICK_PERIOD_IS_1MS
-    #define CPU_MS_TO_TICK(nms)     ( nms )
-    #define CPU_TICK_TO_MS(ntick)   ( ntick )
-#else
-    #define CPU_MS_TO_TICK(nms)     ( (uint32_t)(nms)*CPU_TICK_HZ/1000 )
-    #define CPU_TICK_TO_MS(ntick)   ( (uint32_t)(ntick)*1000/CPU_TICK_HZ )
-#endif
+#define CPU_MS_TO_TICK(nms)     ( (uint32_t)(nms)*CPU_TICK_HZ/1000 )
+#define CPU_TICK_TO_MS(ntick)   ( (uint32_t)(ntick)*1000/CPU_TICK_HZ )
+
+/* 延时函数 ------------------------------------------------------------------*/
+#define cpu_DelayUs(nus)        cpu_DelayCycle( (uint32_t)(nus)*(CPU_TIMER_HZ/1000000) )
+#define cpu_DelayMs(nms)        cpu_DelayCycle( (uint32_t)(nms)*(CPU_TIMER_HZ/1000) )
 
 /* 接口函数 ------------------------------------------------------------------*/
 void cpu_TickInit(void);
-void cpu_TickIRQRegister(TickIRQ_t *irq, tick_t period, TickIRQHandler_t isr);
-void cpu_TickHandler(void);
 void cpu_Delay(uint32_t n);
-void cpu_DelayMs(uint16_t nms);
 
 #endif  /* __CPU_TICK_H */
